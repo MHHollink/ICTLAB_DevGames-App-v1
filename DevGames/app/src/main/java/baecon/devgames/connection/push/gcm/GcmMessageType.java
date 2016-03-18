c3d7c920f81efd6394c2e8aa477a4a6df9ee0c2d
mjollnir94 <marcel_Hollink@hotmail.com>
2016-03-18 13:58:19 +0100

    Moved DevGames App to new Repo

---
+++ DevGames/app/src/main/java/baecon/devgames/connection/push/gcm/GcmMessageType.java
@@ -0,0 +1,6 @@
+package baecon.devgames.connection.push.gcm;
+
+public enum GcmMessageType {
+    PLAIN_NOTIFICATION,
+    NEW_DEVICE_REGISTERED
+}
---
+++ DevGames/app/src/main/java/baecon/devgames/ui/fragment/DevGamesFragment.java
@@ -0,0 +1,10 @@
+package baecon.devgames.ui.fragment;
+
+
+import android.support.v4.app.Fragment;
+
+public abstract class DevGamesFragment extends Fragment {
+    public Fragment getFragment() {
+        return this;
+    }
+}
